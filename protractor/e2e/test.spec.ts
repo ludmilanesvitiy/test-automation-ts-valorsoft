d9f948e77264085dacc9b9a24abf47a369a06398
Valerii <vasilver@gmail.com>
2019-07-13 08:11:47 +0300

    feat(tests): add protractor tests from practical lections

---
+++ protractor/e2e/test.spec.ts
@@ -0,0 +1,10 @@
+// spec.js
+import {browser} from "protractor";
+
+describe('Protractor Demo App', function() {
+    it('should have a title', function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+});
